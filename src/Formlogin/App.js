7b9233bda9192e7eca684cad163cca4cc66c7f7a
arti-jansari <artijansari@gmail.com>
2022-12-27 15:31:17 +0530

    Registration

--- src/Formlogin/App.js
+++ src/Formlogin/App.js
@@ -1,145 +1,15 @@
-import React, { useState } from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import './form.css';
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import Registration from './Registration'
+import  Dashboard from './Dashboard'
 
 const App = () => {
-  const [inputvalue, setInputvalue] = useState({
-    username: "",
-    phone: "",
-    email: "",
-    password: "",
-  });
-
-  const onChangehandler = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-// console.log(name,value)
-    setInputvalue({ ...inputvalue, [name]: value });
-  };
-
-  const [data, setData] = useState([]);
- 
-  const[localvalue,setLocalvalue]=useState([]);
-  const onhandlesubmit = (e) => {
-    e.preventDefault();
-
-    const newdata = { ...inputvalue, id: new Date().getTime().toString() };
- 
-    setData([...data, newdata]);
-    console.log(newdata);
-    setInputvalue({
-      username: "",
-      phone: "",
-      email: "",
-      password: "",
-    })
- localvalue.push(newdata)
-  setLocalvalue(localvalue)
-   console.log(localvalue)
-    localStorage.setItem('localvalue',JSON.stringify(localvalue));
-  }; 
-  //  const onhandleclick = ()=>{
-  //   navigate('/dashboard')
-  //  }
-
-  return (
-    <div className="container-fluid  "  >
-      <div className="row  logo" >
-        <div className="col-lg-4 col-md-6 col-sm-12 bg-info py-5 my-5 mx-auto">
-          <form  action='' onSubmit={onhandlesubmit}>
-            
-            <h2 className="text-center">Registration Form</h2>
-            <div className="form-outline mb-4">
-              <input
-                type="username"
-                autoComplete="off"
-                name="username"
-                value={inputvalue.username}
-                onChange={onChangehandler}
-                id="username"
-                className="form-control"
-              />
-              <label className="form-label" htmlFor="form2Example1">
-                Username
-              </label>
-            </div>
-            <div className="form-outline mb-4">
-              <input
-                type="number"
-                autoComplete="off"
-                name="phone"
-                onChange={onChangehandler}
-                value={inputvalue.phone}
-                id="phone"
-                className="form-control"
-              />
-              <label className="form-label" htmlFor="form2Example1">
-                Phone Number
-              </label>
-            </div>
-            <div className="form-outline mb-4">
-              <input
-                type="emai l"
-                autoComplete="off"
-                name="email"
-                onChange={onChangehandler}
-                value={inputvalue.email}
-                id="email"
-                className="form-control"
-              />
-              <label className="form-label" htmlFor="form2Example1">
-                Email address
-              </label>
-            </div>
-            <div className="form-outline mb-4">
-              <input
-                type="password"
-                id="password"
-                autoComplete="off"
-                name="password"
-                value={inputvalue.password}
-                onChange={onChangehandler}
-                className="form-control"
-              />
-              <label className="form-label" htmlFor="form2Example2">
-                Password
-              </label>
-            </div>
-
-            <button
-              type="submit" 
-                           className="btn btn-light btn-block mx-auto"
-            >
-              Register
-            </button>
-
-            <div className="text-center">
-              <p>
-                Not a member? <a href="#!">Register</a>
-              </p>
-              <p>or sign up with:</p>
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-facebook-f"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-google"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-twitter"></i>
-              </button>
-
-              <button type="button" className="btn btn-link btn-floating mx-1">
-                <i className="fab fa-github"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-          </div>
-  );
+  return(
+<Routes>
+  <Route  to='/' element={<Registration />}></Route>
+  <Route  to='/dashboard' element={<Dashboard />}></Route>
+</Routes>
+  )
 };
 
 export default App;
---
+++ src/Formlogin/Registration.js
@@ -0,0 +1,149 @@
+
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './form.css';
+// import Dashboard from './Dashboard';
+import {useNavigate} from 'react-router-dom'
+
+
+const Registration = () => {
+    const navigate = useNavigate();
+    const [inputvalue, setInputvalue] = useState({
+        username: "",
+        phone: "",
+        email: "",
+        password: "",
+      });
+    
+      const onChangehandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+    // console.log(name,value)
+        setInputvalue({ ...inputvalue, [name]: value });
+      };
+    
+      const [data, setData] = useState([]);
+     
+      const[localvalue,setLocalvalue]=useState([]);
+      const onhandlesubmit = (e) => {
+        e.preventDefault();
+    
+        const newdata = { ...inputvalue, id: new Date().getTime().toString() };
+     
+        setData([...data, newdata]);
+        console.log(newdata);
+        setInputvalue({
+          username: "",
+          phone: "",
+          email: "",
+          password: "",
+        })
+     localvalue.push(newdata)
+      setLocalvalue(localvalue)
+       console.log(localvalue)
+        localStorage.setItem('localvalue',JSON.stringify(localvalue));
+        navigate('/dashboard')
+        
+      }; 
+      
+      return (
+        
+        <div className="container-fluid  "  >
+          <div className="row  logo" >
+            <div className="col-lg-4 col-md-6 col-sm-12 bg-light py-5 my-5 mx-auto">
+              <form  action='' onSubmit={onhandlesubmit}>
+                
+                <h2 className="text-center">Registration Form</h2>
+                <div className="form-outline mb-3 mx-5">
+                  <input
+                    type="username"
+                    autoComplete="off"
+                    name="username"
+                    value={inputvalue.username}
+                    onChange={onChangehandler}
+                    id="username"
+                    className="form-control"
+                  />
+                  <label className="form-label" htmlFor="form2Example1">
+                    Username
+                  </label>
+                </div>
+                <div className="form-outline mb-3 mx-5">
+                  <input
+                    type="number"
+                    autoComplete="off"
+                    name="phone"
+                    onChange={onChangehandler}
+                    value={inputvalue.phone}
+                    id="phone"
+                    className="form-control"
+                  />
+                  <label className="form-label" htmlFor="form2Example1">
+                    Phone Number
+                  </label>
+                </div>
+                <div className="form-outline mb-3 mx-5">
+                  <input
+                    type="emai l"
+                    autoComplete="off"
+                    name="email"
+                    onChange={onChangehandler}
+                    value={inputvalue.email}
+                    id="email"
+                    className="form-control"
+                  />
+                  <label className="form-label" htmlFor="form2Example1">
+                    Email address
+                  </label>
+                </div>
+                <div className="form-outline mb-3 mx-5">
+                  <input
+                    type="password"
+                    id="password"
+                    autoComplete="off"
+                    name="password"
+                    value={inputvalue.password}
+                    onChange={onChangehandler}
+                    className="form-control"
+                  />
+                  <label className="form-label" htmlFor="form2Example2">
+                    Password
+                  </label>
+                </div>
+    
+                <button
+                  type="submit" 
+                               className="btn btn-primary btn-block mx-5"
+                >
+                  Register
+                </button>
+    
+                <div className="text-center">
+                  <p>
+                     <a href="#!">Login</a>
+                  </p>
+                 
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+    
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-google"></i>
+                  </button>
+    
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-twitter"></i>
+                  </button>
+    
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-github"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+              </div>
+      );
+}
+
+export default Registration
